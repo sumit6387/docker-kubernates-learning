e944e62fb0d6119f0172c3ac17f970276a03b6c5
Sumit Kumar <sumitkumar993618@gmail.com>
2025-06-22 23:35:02 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.json({message: "Hey, Docker is working!"});
+});
+
+app.get('/health', (req, res) => {
+  res.json({status: true, message: "Everything working perfectly v2✌️!"});
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+}
+);
