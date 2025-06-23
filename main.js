58010c05b387e831e4e419276c6f121bd1b2701e
Sumit Kumar <sumitkumar993618@gmail.com>
2025-06-23 17:17:30 +0530

    feat: added test route

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.json({message: "Hey, Docker is working !"});
 });
 
+app.get('/test', (req, res) => {
+  res.json({message: "Hey, Docker is working from test route !"});
+});
+
 app.get('/health', (req, res) => {
   res.json({status: true, message: "Everything working perfectly v2✌️!"});
 });
