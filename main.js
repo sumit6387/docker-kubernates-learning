af19f4888183204cf83f87302c7910ba3ff8f016
Sumit Kumar <sumitkumar993618@gmail.com>
2025-06-23 17:11:14 +0530

    fix: minor change

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-  res.json({message: "Hey, Docker is working!"});
+  res.json({message: "Hey, Docker is working !"});
 });
 
 app.get('/health', (req, res) => {
